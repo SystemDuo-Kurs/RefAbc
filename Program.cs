7442c9975a697c1f756585e77b2ea5d7e41d8a37
Dragan Pepic <nedimco2006@gmail.com>
2022-01-22 17:16:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> lista = new();
+
+lista.Add("asd");
+lista.Add("qwe");
+
+int broj = 5;
+Foo(broj);
+Console.WriteLine(broj);
+
+int[] brojevi = { 5 };
+Bar(brojevi);
+Console.WriteLine(brojevi[0]);
+
+void Bar(int[] niz)
+{
+    niz[0]++;
+}
+
+void Foo(int broj)
+{
+    broj++;
+}
